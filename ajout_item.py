c693b214c79a8bf8e2d8f06762a8aa1ace3d6a36
Fares Ousmer <fares.faresousmer@gmail.com>
2025-10-26 20:35:07 +0100

    ajout docstrings et séparation en objet consommable et permanents

--- ajout_item.py
+++ ajout_item.py
@@ -1,5 +1,31 @@
 def add_item(self, item,quantite):
+    """ Ajoute un objet à l'inventaire
+
+    Args:
+        item (): nom de l'attribut à ajouter
+        quantite(int): quantité à ajouter
+
+    Returns:
+        True si l'ajout a bien été fait, False sinon
+    """
+
+    items = {'shovel', 'hammer', 'lockpick', 'metal_detector', 'rabbit_foot'}
+    
+    if not isinstance(quantite, int) or quantite < 0:
+        return False
+
+    # Objets permanents 
+    if item in items :
+        if hasattr(self, items):
+            self.permanent_items.add(item)
+            print(f"{self.item} a été ajouté ")
+            return True
+        return False
+    
+    # Objets consommables
     if hasattr(self, item):
-        setattr(self, item, getattr(self, item) + quantite)
-        print(f"Le joueur a ramassé {quantite} {item} !")
-        
+        if isinstance(getattr(self, item), int):
+            setattr(self, item, getattr(self, item) + quantite)
+            print(f"Le joueur a ramassé {quantite} {item} !")      
+            return True
+        return False
--- gems.py
+++ gems.py
@@ -1,4 +1,12 @@
 def gems (self, nb_gems) :
+    """Ajout de gems :
+
+    Arguments :
+        nb_gems (int): Nombre de gems à ajouter 
+
+    Returns :
+        Retourne True si l'ajout a bien été fait False sinon 
+    """
     if self.gems >= nb_gems : 
         self.gems -= nb_gems
         return True
