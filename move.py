e4873dd94c977dee12f5d48f24f9f6dc2a810fc3
Fares Ousmer <fares.faresousmer@gmail.com>
2025-10-26 22:48:22 +0100

    Docstring du module move.py

--- move.py
+++ move.py
@@ -1,4 +1,10 @@
 def move (self, dep_ligne, dep_colonne) :
+    """ Déplacement du joueur sur la grille
+
+    Args:
+        dep_ligne (int): Déplacement du joueur suivant les lignes 
+        dep_colonne (int): Déplacement du joueur suivant les colonnes 
+    """
     self.pas -= 1 
     self.ligne += dep_ligne
     self.colonne += dep_colonne
